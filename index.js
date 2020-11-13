530906ce95f4dced03a51761d5f89dbda1bee7ee
dev-rezultz-fr <dev@rezultz.fr>
2020-11-13 17:10:00 +0100

    FB conf

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ import "./src/lup-shell.js";
 
 // Document elements
 const shell = document.getElementById("shell");
+
+// Configure Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyC__QXsAOJauebmQ_FG5-HYgX_wFkMhKKc",
+  authDomain: "line-ups.firebaseapp.com",
+  databaseURL: "https://line-ups.firebaseio.com",
+  projectId: "line-ups",
+  storageBucket: "line-ups.appspot.com",
+  messagingSenderId: "508225472141",
+  appId: "1:508225472141:web:2a5026e64508ec72ffc4c4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
